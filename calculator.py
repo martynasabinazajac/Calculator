9b19d9748badeb3b8d32a9e4100a6d3dae579f67
Martyna Zając <martyna.sabina.zajac@gmail.com>
2023-02-27 21:40:07 +0100

    Poprawki wykonane: wykorzystany f - string, zmienna option przekazana do funkcji, zastosowany if_name__

--- calculator.py
+++ calculator.py
@@ -1,28 +1,29 @@
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def calculator(x, y):
+def calculator(option, x, y):
     if option == 1:
-        logging.info('Dodaję: %0.2f i %0.2f' % (x, y))
+        logging.info(f'Dodaję: {x} i {y}')
         return x + y
     elif option == 2:
-        logging.info('Odejmuję: %0.2f od %0.2f' % (x, y))
+        logging.info(f'Odejmuję: od {x} liczbę {y}')
         return x - y
     elif option == 3:
-        logging.info('Mnożę: %0.2f i %0.2f' % (x, y))
+        logging.info(f'Mnożę: {x} i {y}')
         return x * y
     elif option == 4:
-        logging.info('Dzielę: %0.2f przez %0.2f' % (x, y))
+        logging.info(f'Dzielę: {x} przez {y}')
         return x / y
     else:
         print('Operacja nie możliwa. Brak wybranego działania z dostepnych opcji.')
         exit(1)
 
-
-option = int(input(
+if __name__=="__main__":
+    option = int(input(
     'Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
-x = float(input("podaj pierwsza liczbę:"))
-y = float(input("podaj druga liczbę:"))
-result = calculator(x, y)
-print('Wynik to: %0.2f' %result)
+    x = float(input("podaj pierwsza liczbę:"))
+    y = float(input("podaj druga liczbę:"))
+    result = calculator(option, x, y)
+    print(f'Wynik to: {result}')
